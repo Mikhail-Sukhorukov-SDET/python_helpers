8b561d0ed924fed6f828da8a655fd0a129a23c18
Mikhail Sukhorukov <Mikhail_Sukhorukov@epam.com>
2021-04-16 18:23:43 +0300

    update of functional

--- python_helper_functional.py
+++ python_helper_functional.py
@@ -1,6 +1,7 @@
 print(list(map(lambda x: x * 2, range(8))))  # map
 print(list(filter(lambda x: x % 2 != 0, range(10))))  # filter
 print(list(zip(["dsfs", "sdfsdfas", "asdfsadfs"], range(3))))  # zip
+
 # генераторы списков, множевств и словарей
 print([x * x for x in range(10) if x % 2 != 0])  # list
 print({x * x for x in range(10) if x % 2 != 0})  # set
