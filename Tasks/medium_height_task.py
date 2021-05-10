cdd19d950b7972f2ec4fe42b4f0a55b26ea947ea
Mikhail-Sukhorukov-EPAM <Mikhail_Sukhorukov@epam.com>
2021-05-10 20:38:09 +0300

    last task of python beginner course

---
+++ Tasks/medium_height_task.py
@@ -0,0 +1,15 @@
+m = {}
+with open("Tasks/dataset_3380_5.txt") as file:
+    for row in file:
+        row = row.split()
+        class_key = int(row[0])
+        if int(class_key) not in m.keys():
+            m[class_key] = [0, 0]
+        m[class_key][0] += int(row[2])
+        m[class_key][1] += 1
+keys = sorted(m.keys())
+for key in range(1, len(keys)+1):
+    if key not in m.keys():
+        print(f"{key} -")
+    else:
+        print(key, m[key][0]/m[key][1])
