d9dc5510ee6dfd48c5f6ebe023e878a87a4d0520
Mikhail-Sukhorukov-EPAM <Mikhail_Sukhorukov@epam.com>
2021-05-15 18:50:35 +0300

    sorting by key

--- python_helper_functional.py
+++ python_helper_functional.py
@@ -7,3 +7,8 @@ print([x * x for x in range(10) if x % 2 != 0])  # list
 print({x * x for x in range(10) if x % 2 != 0})  # set
 date = {"name": "Misha", "dreams": None, "money": "a little bit"}
 print({k: v for k, v in date.items() if v})  # dict
+
+d = {'Джек': 2.5, 'Билл': 4.0}
+# сортировака в первую очерердь от большего к меньшему по значениям и во вторую по алфавиту по ключам
+for item in sorted(d.items(), key= lambda kv: (-kv[1], kv[0])):
+    print(item[0], item[1])
