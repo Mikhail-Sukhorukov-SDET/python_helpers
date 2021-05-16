8e67ba0f13398c58660926eaa11a78767036f4a0
Mikhail-Sukhorukov-EPAM <Mikhail_Sukhorukov@epam.com>
2021-05-16 16:52:57 +0300

    decorator: memoized with maxsize

--- python_helper_decorator.py
+++ python_helper_decorator.py
@@ -67,12 +67,12 @@ print(identity(5), "@introduce_on_debug\n\n")
 
 
 def optional_introduce(func):
-
     @functools.wraps(func)
     def inner(*args, introduce=False, **kwargs):
         if introduce:
             print(func.__name__)
         return func(*args, **kwargs)
+
     return inner
 
 
@@ -83,4 +83,72 @@ def example(x):
 
 print(example(5), "@optional_introduce")
 print(example(7, introduce=True), "@optional_introduce")
-print(example(9, introduce=False), "@optional_introduce")
+print(example(9, introduce=False), "@optional_introduce\n\n")
+
+
+def bucket(*decorator_args, **decorator_kwargs):
+    def wrap(func):
+        @functools.wraps(func)
+        def inner(*args, **kwargs):
+            print((decorator_args, decorator_kwargs))
+            return func(*args, **kwargs)
+
+        return inner
+
+    return wrap
+
+
+@bucket(1, 2, 3, [1, 2, 3], 'one', 'two', 'three', one=1, two=2, three=3)
+def identity1(x):
+    return x
+
+
+@bucket()
+def identity2(x):
+    return x
+
+
+print(identity1(42))
+print(identity2(42), "\n\n")
+
+
+# maxsize - максимальный размер словаря
+def memoized(maxsize=None):
+    def inner(func):
+
+        memory = {}
+
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            key = (args, tuple(sorted(kwargs.items())))
+            if not maxsize or len(memory) < maxsize:
+                if key not in memory:
+                    memory[key] = func(*args, **kwargs)
+                return memory[key]
+            else:
+                if key not in memory:
+                    memory.popitem()
+                    memory[key] = func(*args, **kwargs)
+                return memory[key]
+
+        return wrapper
+
+    return inner
+
+
+@memoized(maxsize=2)
+def sum_of_two(a, b):
+    print(a, b, end='; ')
+    return a + b
+
+
+print(sum_of_two(2, 0), '\n')
+print(sum_of_two(2, 0), '\n')
+
+print(sum_of_two(4, 2), '\n')
+print(sum_of_two(4, 2), '\n')
+
+print(sum_of_two(5, 0), '\n')
+print(sum_of_two(5, 0), '\n')
+
+print(sum_of_two(4, 2))
