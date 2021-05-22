7494aad4a6883de3f5c614aceb13f3638ae228fe
Mikhail-Sukhorukov-EPAM <Mikhail_Sukhorukov@epam.com>
2021-05-22 23:10:02 +0300

    задачка про лямбду

--- functional.py
+++ functional.py
@@ -10,7 +10,7 @@ print({k: v for k, v in date.items() if v})  # dict
 
 d = {'Джек': 2.5, 'Билл': 4.0}
 # сортировака в первую очерердь от большего к меньшему по значениям и во вторую по алфавиту по ключам
-for item in sorted(d.items(), key= lambda kv: (-kv[1], kv[0])):
+for item in sorted(d.items(), key=lambda kv: (-kv[1], kv[0])):
     print(item[0], item[1])
 
 # выражение генератор - можно обойти только один раз
@@ -20,3 +20,20 @@ for i in a:
 
 # экономит память - элементы не хранятся в памяти - в списке такое не сохранить
 c = (i for i in range(10000000000))
+
+""" 
+Реализуйте функцию mod_checker(x, mod=0), которая будет генерировать лямбда функцию от одного аргумента y, 
+которая будет возвращать True, если остаток от деления y на x равен mod, и False иначе. """
+
+
+def mod_checker(x, mod=0):
+    return lambda y: y % x == mod
+
+
+mod_3 = mod_checker(3)
+
+print(mod_3(3))  # True
+print(mod_3(4))  # False
+
+mod_3_1 = mod_checker(3, 1)
+print(mod_3_1(4))  # True
