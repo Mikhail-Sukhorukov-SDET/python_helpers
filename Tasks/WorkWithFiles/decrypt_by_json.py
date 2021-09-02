db423c27b2068f8df98b872d1986580db16311ab
Mikhail-Sukhorukov-EPAM <Mikhail_Sukhorukov@epam.com>
2021-09-02 21:26:22 +0300

    расшифровка файла через джсон файл

---
+++ Tasks/WorkWithFiles/decrypt_by_json.py
@@ -0,0 +1,10 @@
+import json
+
+ENCRYPTED_FILE, KEYS_FILE = "Data/Abracadabra.txt", "Data/Alphabet.json"
+with open(ENCRYPTED_FILE) as enc, open(KEYS_FILE) as keys_file:
+    keys = json.load(keys_file)
+    for symbol in enc.read():
+        if symbol.isalpha():
+            print(keys[symbol], end="")
+        else:
+            print(symbol, end="")
