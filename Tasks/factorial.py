6f21d183df75a27853ed0b626d2b4dccc5007d8c
Mikhail-Sukhorukov-EPAM <Mikhail_Sukhorukov@epam.com>
2022-01-15 23:52:18 +0300

    trailing_zeros

--- Tasks/factorial.py
+++ Tasks/factorial.py
@@ -1,12 +1,31 @@
 def factorial(n):
     fact = 1
-    for i in range(1, n+1):
-        fact *=i
+    for i in range(1, n + 1):
+        fact *= i
     return fact
+
+
 print(factorial(5))
+
+
+def trailing_zeros(n: int) -> int:
+    n = factorial(n)
+    last_number, counter = 0, 0
+    while last_number == 0:
+        last_number = n % 10
+        n //= 10
+        if last_number == 0:
+            counter += 1
+    return counter
+
+
+print(trailing_zeros(20))
+
 
 def fact(n):
     if n == 1:
         return 1
-    return fact(n-1) * n
+    return fact(n - 1) * n
+
+
 print(fact(4))
